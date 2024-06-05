109e5e49955aba39b1dd119083d7f2a8e935fb91
Bodhan Dhakal <bodhandhakal22@gmail.com>
2024-06-05 13:28:19 +0545

    learned dependant queries

--- src/dependant.jsx
+++ src/dependant.jsx
@@ -18,10 +18,13 @@ const Dependant = () => {
         queryKey: ['post'],
         queryFn: () => fetchPostById(2),
     });
+     const postId = post?.id;
 
-    const { data: comments } = useQuery({
+    const { data: comments, status } = useQuery({
         queryKey: ['comments', post?.id],
         queryFn: () => fetchCommentsByPostId(post.id),
+        //!!false 
+        enabled:!! postId,
     });
 
     return (
@@ -29,7 +32,7 @@ const Dependant = () => {
             <h1 className="text-lg font-bold">Post:</h1>
             {isLoading ? <p>Loading the post</p> : <h2>{post?.title}</h2>}
             <br />
-            <h1 className="text-lg font-bold">Comments</h1>
+            <h1 className="text-lg font-bold">Comments: {status === 'pending' &&  'PENDING'} </h1>
             <ul>
                 {comments?.map((comment) => (
                     <p key={comment.id}>{comment.body}</p>
