2e73361b631bdac93647d763f7f8e989078ed589
Bodhan Dhakal <bodhandhakal22@gmail.com>
2024-06-05 12:48:48 +0545

    learneed parallel queries

--- src/paginated.jsx
+++ src/paginated.jsx
@@ -9,6 +9,7 @@ function Products() {
     const skip = parseInt(searchParams.get('skip') || 0); // Initial limit set to 4
     const limit = parseInt(searchParams.get('limit') || 0);
     const q = searchParams.get('q') || '';
+    const category =searchParams.get('category') || '';
 
 
 
@@ -22,13 +23,19 @@ function Products() {
     });
 
     const { data: products } = useQuery({
-        queryKey: ['products', limit,skip, q],
+        queryKey: ['products', limit,skip, q, category],
         queryFn: async () => {
-            const data = await fetch(`https://dummyjson.com/products/search?limit=${limit}&skip=${skip}&q=${q}`)
+            let url =`https://dummyjson.com/products/search?limit=${limit}&skip=${skip}&q=${q}`
+            if(category){
+                url = `https://dummyjson.com/products/category/${category}?limit=${limit}&skip=${skip}`
+            }
+
+            return await fetch(url)
             .then((res) => res.json());
-            return data.products;
+          
         },
         placeholderData: keepPreviousData,
+        staleTime: 20000
     });
 
     const handleMove= (moveCount)=>{
@@ -68,6 +75,7 @@ function Products() {
                                   setSearchParams((prev)=>{
                                     prev.set('q', e.target.value);
                                     prev.set('skip',0);
+                                    prev.delete('category')
                                     return prev
 
                                   })
@@ -78,7 +86,14 @@ function Products() {
                                 className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="IPhone"
                             />
-                            <select className="border p-2" onChange={() => {}}>
+                            <select className="border p-2" onChange={(e) => {
+                                setSearchParams((prev) =>{
+                                    prev.set('skip', 0)
+                                    prev.delete('q')
+                                    prev.set('category', e.target.value)
+                                    return prev
+                                })
+                            }}>
                                 <option>Select category</option>
                                 {categories?.map((category) => (
                                     <option key={category.slug} value={category.slug}>
@@ -90,7 +105,7 @@ function Products() {
                     </div>
 
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                        {products?.map((product) => (
+                        {products?.products?.map((product) => (
                             <div key={product.id} className="group relative">
                                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-64">
                                     <img
@@ -124,11 +139,14 @@ function Products() {
 
                     <div className="flex gap-2 mt-12">
                         <button
+                        disabled={skip < limit}
                             className="bg-purple-500 px-4 py-1 text-white rounded"
                             onClick={()=> {handleMove(-limit)} }>
                             Prev
                         </button>
                         <button
+                        //pagination done
+                        disabled={limit + skip >= products?.total}
                             className="bg-purple-500 px-4 py-1 text-white rounded"
                             onClick={()=> {handleMove(limit)} }>
                             Next
--- src/parallel.jsx
+++ src/parallel.jsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import { useQuery, useQueries } from '@tanstack/react-query';
+import React from "react";
+import { useQuery,  useQueries } from "@tanstack/react-query";
 
 const Parallel = () => {
-    const [userIds, setUserIds] = React.useState([1]);
+  const [userIds, setUserIds] = React.useState([1, 2]);
 
-    return (
-        <div>
-            <button
-                onClick={() =>
-                    setUserIds((prev) => {
-                        return [...prev, Date.now()];
-                    })
-                }>
-                Load more
-            </button>
 
-            {userIds.map((id) => (
-                <h1 key={id}>{id}</h1>
-            ))}
-        </div>
-    );
+  //learing paralllel queries react quereis cant use callback
+  // userIds.forEach((id)=>{
+  //     const userQuery = useQuery({
+
+  //         queryKey: ['user' ,id],
+  //         queryFn: async () => {
+  //             const data = await fetch(`https://dummy.json.com/users/${id}`).then(res=> res.json()
+  //         );
+  //         return data;
+  //         },
+  //     });
+  // });
+
+  const userQueries = useQueries({
+    queries: userIds.map((id) => {
+      return {
+        queryKey: ['user', id],
+        queryFn: async () => {
+          const data = await fetch(`https://dummy.json.com/users/${id}`).then(
+            (res) => res.json()
+          );
+          return data;
+        },
+      };
+    }),
+  });
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          setUserIds((prev) => {
+            return [...prev, Date.now()];
+          })
+        }
+      >
+        Load more
+      </button>
+
+      {userIds.map((id) => (
+        <h1 key={id}>{id}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default Parallel;
